6c8d270da4c98e3f6170d726417419203f8b874e
Devashish Sood <devashishsood9@gmail.com>
2025-11-16 20:23:51 -0500

    bones

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+open Wyag.Init
+open Cmdliner 
+
+let main () = Cmd.eval init_cmd
+let () = if !Sys.interactive then () else exit (main ())
---
+++ lib/init.ml
@@ -0,0 +1,15 @@
+open Cmdliner
+open Cmdliner.Term.Syntax
+
+let init path = Printf.printf "Initializing repository at %s\n" path
+
+let path =
+  let doc = "path at which repository is to be initialized" in
+  Arg.(value & pos 0 string "." & info [] ~doc ~docv:"path")
+
+let init_cmd =
+  let doc = "initialize a git repository" in
+  Cmd.make (Cmd.info "init" ~doc)
+  @@
+  let+ path in
+  init path
